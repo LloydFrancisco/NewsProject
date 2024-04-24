84229791771ff3e682d3be8d0ee1d4b78310d6c0
LloydFrancisco <lloyddaenielfrancisco@gmail.com>
2024-04-25 04:38:42 +0800

    News API

--- model.js
+++ model.js
@@ -8,7 +8,7 @@ const model = {
 
     async fetchNews(query, page) {
         try {
-            const url = `https://newsapi.org/v2/top-headlines?country=us&q=${query}&from=2024-04-24&pageSize=20&page=${page}&sortBy=popularity&apiKey= 507bf870e8cf4463aa1628e7926758d1`;
+            const url = `https://newsapi.org/v2/top-headlines?country=us&q=${query}&from=2024-04-24&pageSize=20&page=${page}&sortBy=popularity&apiKey= 83ed4c33713144889c2d63f4ee417f59`;
             // 83ed4c33713144889c2d63f4ee417f59
             // 507bf870e8cf4463aa1628e7926758d1
             const response = await fetch(url);
